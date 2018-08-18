f43d03a38e2fac51cfd2dc93be0175517aeb7bc8
yoyoye <yoyorei@gmail.com>
2018-08-18 00:44:08 -0400

    pybank main

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,28 +12,52 @@ with open(csvpath, newline="") as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    csv_header = next(csvreader)
+
     for row in csvreader:
 
         date.append(row[0])
 
         pl.append((row[1]))
 
-date.pop(0)
-
-pl.pop(0)
-
 total = 0
 
 for num in pl:
 
-    total = total +float(num)
+    total = total +int(num)
 
 average = (int(pl[len(pl) -1]) - int(pl[0]))/(len(date))
 
-print(len(date))
+max = pl[0]
 
-print(len(pl))
+min = pl[0]
 
-print(total)
+for num in pl:
 
-print(average)
+    if int(num) > int(max):
+
+        max = num
+    
+    elif int(num) < int(min):
+
+        min = num
+
+maxdate = date[pl.index(max)]
+
+mindate = date[pl.index(min)]
+
+print("")
+
+print("Financial Analysis")
+
+print("--------------------------------------------------")
+
+print("Total Months: "+str(len(date)))
+
+print("Total: " +"$" + str(total))
+
+print("Average Change: " + "$" + str(round(average,2)))
+
+print("Greatest Increase in Profits: " +maxdate + " (" + max + ")")
+
+print("Greatest Decrease in Profits: " +mindate + " (" + min + ")")
